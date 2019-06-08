ee7d79833e7d17ca79d610eee2d7631d1a8face7
Frank-Woods <49346787+Frank-Woods@users.noreply.github.com>
2019-06-08 22:58:01 +0400

    test

---
+++ js/tests.js
@@ -0,0 +1,38 @@
+res.onclick = function result() {
+	if (document.getElementById("block1")) {
+		document.getElementById("block1").remove();
+	}
+	var name = ""; 
+	var flag = true;
+	var	block1 = document.createElement("div");
+	block1.id = "block1";
+	block1.className = "main_container";
+	var	block2 = document.createElement("div");
+	block2.className  = "container";
+	var questions = document.getElementsByName('q');
+	for (var i = 0; i < questions.length; i++) {
+		var inputs = document.getElementsByName('q' + (i+1));
+		for(var j = 0; j < inputs.length; j++) {
+			if (inputs[j].checked) {
+				name += inputs[j].value + "_";
+				flag = false;
+				break;
+			}
+		}
+		if (flag) {
+			var text = document.createElement("p");
+			text.style.color = "red";
+			text.innerHTML = "Пожалуйста ответьте на все вопросы!";
+			block2.appendChild(text);
+			block1.appendChild(block2);
+			document.getElementsByTagName("main")[0].appendChild(block1);
+			return;
+		}	
+		flag = true;
+	}
+	var	content = new Image();
+	content.src = "img/" + name + ".jpg";
+	block2.appendChild(content);
+	block1.appendChild(block2);
+	document.getElementsByTagName("main")[0].appendChild(block1);
+}
---
+++ js/theme.js
@@ -0,0 +1,19 @@
+var theme = document.getElementById('theme');
+theme.onclick = function theme() {
+	var theme = document.getElementById('theme');
+	var head = document.getElementsByTagName('head')[0];
+	var black = document.createElement("link");
+	if (theme.innerHTML == "Темная тема") {
+		localStorage.setItem('theme', 'black');
+		black.setAttribute('id', 'black');
+		black.href = "css/styleBlack.css";
+		black.rel = "stylesheet";
+		black.type = "text/css";
+		head.appendChild(black);
+		theme.innerHTML = "Светлая тема"; 
+	}else{
+		document.getElementById("black").remove();
+		theme.innerHTML = "Темная тема"
+		localStorage.setItem('theme', 'white');
+	}
+}
